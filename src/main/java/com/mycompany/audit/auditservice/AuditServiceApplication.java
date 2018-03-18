232e933871d5cc5f2c7212155571a50b09da8831
Colin But <colinbut@users.noreply.github.com>
2018-03-18 23:51:07 +0000

    Initial commit

---
+++ src/main/java/com/mycompany/audit/auditservice/AuditServiceApplication.java
@@ -0,0 +1,12 @@
+package com.mycompany.audit.auditservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditServiceApplication.class, args);
+	}
+}
