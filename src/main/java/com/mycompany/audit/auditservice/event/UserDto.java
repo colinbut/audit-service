f5b6d17eeea061bb258b07a98a94deee46d7763d
Colin But <colinbut@users.noreply.github.com>
2018-03-19 22:47:29 +0000

    UserDTO exactly same as one from UserService

---
+++ src/main/java/com/mycompany/audit/auditservice/event/UserDto.java
@@ -0,0 +1,27 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2018 Colin But. All rights reserved. 
+ * |-------------------------------------------------
+ */
+package com.mycompany.audit.auditservice.event;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * Holds the same info as UserDto from UserService
+ *
+ * DDD (Domain Driven Design) entails:
+ *
+ * So due to bounded countext the UserDto is duplicated across service
+ * as this particular UserDto here represents the context within Audit domain context.
+ */
+@Data
+public class UserDto {
+    private String ssn;
+    private String firstname;
+    private String secondname;
+    private Date dob;
+    private AddressDto address;
+}
