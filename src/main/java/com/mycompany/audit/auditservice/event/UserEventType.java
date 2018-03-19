d3eefaadcd8b288bfe56b346cf953941655bfcd7
Colin But <colinbut@users.noreply.github.com>
2018-03-19 22:48:40 +0000

    The UserEventType

---
+++ src/main/java/com/mycompany/audit/auditservice/event/UserEventType.java
@@ -0,0 +1,12 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2018 Colin But. All rights reserved. 
+ * |-------------------------------------------------
+ */
+package com.mycompany.audit.auditservice.event;
+
+public enum UserEventType {
+    CREATED,
+    UPDATED,
+    DELETED
+}
