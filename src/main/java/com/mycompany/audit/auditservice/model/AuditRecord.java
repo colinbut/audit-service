23b7bcdfb5902c929b64ba08f7c8ffaa695069de
Colin But <colinbut@users.noreply.github.com>
2018-03-19 22:49:32 +0000

    The model AuditRecord

---
+++ src/main/java/com/mycompany/audit/auditservice/model/AuditRecord.java
@@ -0,0 +1,25 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2018 Colin But. All rights reserved. 
+ * |-------------------------------------------------
+ */
+package com.mycompany.audit.auditservice.model;
+
+import com.mycompany.audit.auditservice.event.UserEventType;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class AuditRecord {
+
+    private int id;
+    private UserEventType userEventType;
+    private String ssn;
+    private String firstName;
+    private String secondName;
+    private Date dateOfBirth;
+
+
+
+}
